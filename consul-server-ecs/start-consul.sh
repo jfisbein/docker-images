e46ad81fa5fa646b753cb4570b13e84c01517ea9
Joan Fisbein <joan@fon.com>
2015-12-22 15:50:30 +0100

    Added consul-server-ecs image

--- consul-server-ecs/start-consul.sh
+++ consul-server-ecs/start-consul.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env sh
-curl -s http://169.254.169.254/latest/meta-data/local-ipv4
+LOCAL_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+INTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | head -c -1`
+
+/bin/consul $* -node="$INTANCE_ID" -advertise="$LOCAL_IP" -dc="$AZ"
